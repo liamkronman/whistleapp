8eca409227dce16de8dd0d67801071b1c850f80c
liamkronman <liamkronman@gmail.com>
2022-07-13 17:39:34 -0700

    added left chevron to userfeature

--- src/app/UserFeature.js
+++ src/app/UserFeature.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import { ChevronLeft } from "react-native-feather";
+import { StyleSheet, View, Dimensions } from "react-native";
 import UserDisplay from '../components/UserDisplay';
 
 const UserFeature = ({ route, navigation }) => {
     const { username } = route.params;
 
     return (
-        <UserDisplay isOwner={false} navigation={navigation} username={username} />
+        <View style={{ width: Dimensions.get('window').width, height: Dimensions.get('window').height - 170, backgroundColor: '#ECEEFF' }}>
+            <UserDisplay isOwner={false} navigation={navigation} username={username} />
+            <ChevronLeft onPress={() => navigation.pop()} style={styles.backBtn} width={34} height={34}  />
+        </View>
     );
 }
 
 export default UserFeature;
+
+const styles = StyleSheet.create({
+    backBtn: {
+        position: 'absolute',
+        alignSelf: 'flex-start', 
+        top: 12, 
+        left: 12,
+        color: '#2C65F6'
+    }
+});
