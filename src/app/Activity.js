3b1e1af2886022c53ad1f09c847d1c72ae6a2f31
liamkronman <liamkronman@gmail.com>
2022-07-20 00:23:25 -0700

    activity page started

--- src/app/Activity.js
+++ src/app/Activity.js
@@ -1,9 +1,47 @@
 import * as React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, Image, ScrollView } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Image, FlatList, Dimensions } from 'react-native';
+import axios from 'axios';
+import { selectAccessToken } from '../redux/slices/authSlice';
+import { useSelector } from 'react-redux';
 
 const Activity = ({ navigation }) => {
+    const jwtToken = useSelector(selectAccessToken);
+    const [notifications, setNotifications] = React.useState([]);
+
+    React.useEffect(() => {
+        axios.post("https://trywhistle.app/api/user/getnotifications", {}, {
+            headers: {
+                "x-access-token": jwtToken
+            }
+        })
+        .then(resp => {
+            setNotifications(notifications => [...notifications, ...resp.data.notifications]);
+        })
+        .catch(err => {
+            console.log(err);
+        });
+    }, []);
+
+    const renderNotification = ( notification ) => {
+        console.log(notification);
+        return (
+            <></>
+        );
+    }
+
     return (
         <View style={styles.container}>
+            <View style={styles.headerContainer}>
+                <Text style={styles.headerText}>Activity</Text>
+            </View>
+            {
+                notifications.length > 0
+                ? <FlatList
+                data={notifications}
+                renderItem={renderNotification}
+                />
+                : <Text style={styles.noNotificationsText}>No notifications yet!</Text>
+            }
         </View>
     );
 };
@@ -15,6 +53,25 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#ECEEFF',
         alignItems: 'center',
-        justifyContent: 'center',
     },
+    headerContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 70,
+        width: Dimensions.get('window').width,
+        borderBottomWidth: 3,
+        borderBottomColor: '#93B9F2'
+    },
+    headerText: {
+        fontFamily: 'WorkSans-Bold',
+        fontSize: 30,
+        color: '#2C65F6'
+    },
+    noNotificationsText: {
+        fontFamily: 'WorkSans-Regular',
+        fontSize: 18,
+        color: '#6187EB',
+        marginBottom: 4,
+        marginTop: 20
+    }
 });
